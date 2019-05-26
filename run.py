a6940cb3c718b0b830c5f9f38a403831986b39c9
KnowHow866 <accidental001@gmail.com>
2019-05-26 13:42:59 +0800

    add run.py command

--- run.py
+++ run.py
@@ -10,8 +10,9 @@ from app.model.loader import Loader
 from argparse import ArgumentParser
 parser = ArgumentParser(description=__doc__)
 parser.add_argument('-c', '--connect_to_db', dest='connection_db', help='check DB health or create DB, provide args is DB name')
-parser.add_argument('-l', '--load_data', dest='data_path', help='Load given path data to db, json format only')
+parser.add_argument('-l', '--load_data', dest='data_path', help='Load given path data to db (json format only) and traverse database')
 parser.add_argument('-e', '--echo_database', dest='echo_database', help='true or false, echo database opperation')
+parser.add_argument('-t', '--traverse_database', dest='traverse_database', help='true or false, traverse_database and print all data')
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -28,3 +29,5 @@ if __name__ == '__main__':
     if args.data_path is not None: 
         Loader.load(file_path=args.data_path)
         Loader.traverse_database()
+
+    if args.traverse_database in ('true', 'True'): Loader.traverse_database()
