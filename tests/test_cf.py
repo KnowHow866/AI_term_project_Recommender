59a2c52fbdcaaacc97963d05b731e52690be2263
grceliu <a196482468@gmail.com>
2019-06-10 20:50:40 +0800

    feat(cf)

--- tests/test_cf.py
+++ tests/test_cf.py
@@ -71,42 +71,17 @@ def test_recommend():
 
 def test_cf_recommend_class():
     class CollaborativeFiltering(AlgorithmAbstraction):
-        def __init__(self):
-            self.session = DBManager.get_session()
-            self.Food = pd.read_sql(session.query(Food).statement,session.bind)
-            self.User = pd.read_sql(session.query(User).statement,session.bind)
-            self.Review = pd.read_sql(session.query(UserRecommendationReview).statement,session.bind)
-            self.food = pd.read_sql(session.query(Food).statement,session.bind).rename(columns={'id': 'food_id'})
-            self.user = pd.read_sql(session.query(User).statement,session.bind).rename(columns={'id': 'user_id'})
-            self.review = pd.read_sql(session.query(UserRecommendationReview).statement,session.bind)
-
-        def _prediction_for_all(self):
-            self.review['is_accept'] = np.where(self.review['is_accept']==True, 1, -1)
-            rating_pivot = self.review.pivot(index='user_id',columns = 'food_id',values='is_accept').fillna(0)
-
-            rating_df = rating_pivot.as_matrix()
-            user_ratings_mean = np.mean(rating_df, axis = 1)
-            r_demeaned = rating_df - user_ratings_mean.reshape(-1, 1)
-
-            # Matrix Factorization
-            U, sigma, Vt = svds(r_demeaned, k = 50)
-            sigma = np.diag(sigma)
-
-            prediction_all= np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
-            prediction_df = pd.DataFrame(prediction_all, columns = rating_pivot.columns)
-            return prediction_df
-
-        def _recommend_for_user(self, prediction_df, user_id, num_recommendations = 10):
+        def _recommend_for_user(prediction_df, user_id, food, review, num_recommendations = 10):
             sorted_user_predictions = prediction_df.iloc[user_id - 1].sort_values(ascending=False)
             user_preference = pd.DataFrame(sorted_user_predictions).reset_index()
             user_preference.columns = ['food_id', 'ratings']
 
-            user_review = self.review[self.review["user_id"] == user_id]
-            food_not_yet_recommended = self.food[~self.food['food_id'].isin(user_review['food_id'])]
+            user_review = review[review["user_id"] == user_id]
+            food_not_yet_recommended = food[~food['food_id'].isin(user_review['food_id'])]
             recommend_list = pd.merge(food_not_yet_recommended, user_preference, on = "food_id").sort_values(by = ["ratings"], ascending = False).iloc[:num_recommendations]["food_id"].to_list()
 
             if len(recommend_list) < 10:
-                recommend_list.extend(user_preference.iloc[:(10-len(recommend_list))]["food_id"].to_list())
+                recommned_list.extend(user_preference.iloc[:(10-len(recommend_list))]["food_id"].to_list())
 
             return recommend_list
 
@@ -116,10 +91,31 @@ def test_cf_recommend_class():
             food_list = [food_map[id] for id in recommendation_list]
             return food_list
 
-        def recommend(self, *args, **kwargs) -> 'Food[] , high recommendation prority in lower index ':
+        def recommend(self, user=None, *args, **kwargs) -> 'Food[] , high recommendation prority in lower index ':
             ''' Everytime client ask for recommendation will invoke this method, return a python object '''
-            user_id = 130
-            prediction_df = self._prediction_for_all()
-            recommendation_list = self._recommend_for_user(prediction_df, user_id)
+            user_id = user.id
+            
+            session = DBManager.get_session()
+            food = pd.read_sql(session.query(Food).statement,session.bind).rename(columns={'id': 'food_id'})
+            user = pd.read_sql(session.query(User).statement,session.bind).rename(columns={'id': 'user_id'})
+            review = pd.read_sql(session.query(UserRecommendationReview).statement,session.bind)
+
+            # Data processing & prepare
+            review['is_accept'] = np.where(review['is_accept']==True, 1, -1)
+            rating_pivot = review.pivot(index='user_id',columns = 'food_id',values='is_accept').fillna(0)
+
+            # Turn review to ratings matrix
+            rating_df = rating_pivot.as_matrix()
+            user_ratings_mean = np.mean(rating_df, axis = 1)
+            r_demeaned = rating_df - user_ratings_mean.reshape(-1, 1)
+
+            # Matrix factorization
+            U, sigma, Vt = svds(r_demeaned, k = 50)
+            sigma = np.diag(sigma)
+
+            prediction_all= np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
+            prediction_df = pd.DataFrame(prediction_all, columns = rating_pivot.columns)
+
+            recommendation_list = self._recommend_for_user(prediction_df, user_id, food, review)
             food_list = self._id_to_food_list(recommendation_list)
             return food_list
