b82be558874b534413ae3df494fd2278e2606674
KnowHow866 <accidental001@gmail.com>
2019-06-13 01:43:51 +0800

    feat(user proxy)

--- app/algorithm/evaluation/user_proxy_collection.py
+++ app/algorithm/evaluation/user_proxy_collection.py
@@ -6,12 +6,14 @@ Provide a userProxy collection for client to
 from .simple_user_proxy import SimpleUserproxy
 from .keyword_user_proxy import KeywordUserProxy
 from .keyword_enhance_user_proxy import KeywordEnhanceUserProxy
+from .proxy_for_remember_preference import ProxyForRememberPreference
 
 class UserProxyCollection():
     default_proxy = KeywordEnhanceUserProxy
     proxys = (
         SimpleUserproxy,
         KeywordUserProxy,
-        KeywordEnhanceUserProxy
+        KeywordEnhanceUserProxy,
+        ProxyForRememberPreference
     )
     
