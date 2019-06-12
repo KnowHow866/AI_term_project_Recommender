21565c9dbdf34d47bd2301b2c364ffdbd27a42ab
KnowHow866 <accidental001@gmail.com>
2019-06-12 23:11:56 +0800

    feat(add ContentBasedFiltering to commane line)

--- app/algorithm/collection.py
+++ app/algorithm/collection.py
@@ -8,12 +8,14 @@ from .random_algo import RandomAlgorithm
 from .composite import FairAlgorithmComposite
 from .lose_one_kg import LoseOneKgSchedule
 from .markov_lose_one_kg import ModelBasedAgentOfMarkovVerionLoseOneKg
+from .content_based_filtering import ContentBasedFiltering
 
 class AlgorithmCollection():
     default_algo = FairAlgorithmComposite
     algos = (
         RandomAlgorithm, 
         CollaborativeFiltering,
+        ContentBasedFiltering,
         LoseOneKgSchedule,
         ModelBasedAgentOfMarkovVerionLoseOneKg,
         FairAlgorithmComposite
