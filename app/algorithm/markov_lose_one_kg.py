40144418f6d4336ff0dabd19c5c4ab2a41921af4
KnowHow866 <accidental001@gmail.com>
2019-06-13 10:48:14 +0800

    doc

--- app/algorithm/markov_lose_one_kg.py
+++ app/algorithm/markov_lose_one_kg.py
@@ -47,7 +47,7 @@ class ModelBasedAgentOfMarkovVerionLoseOneKg(AlgorithmAbstraction):
             return_list.append(food_list[i])
 
         return return_list
-        
+
     def recommend(self, user=None):
         if self.first_time:
             self.first_time = False
@@ -81,8 +81,7 @@ class ModelBasedAgentOfMarkovVerionLoseOneKg(AlgorithmAbstraction):
             except:
                 self.target_cal += self.session.query(Food).filter(Food.id == food_taken_id).first().calories - last_requirement 
 
-        # 開始處理
-        
+        # 開始處理        
         requirement = self.cal_requirement(self.time)
         this_meal_target = int( self.target_cal / self.meals_left )
         r_list = self.recommend_food_with_possibility(this_meal_target, requirement, self.foodlist)
